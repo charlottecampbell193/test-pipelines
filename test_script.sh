b8bfee54dd0bd52c1c58b02620069321a8fa6ab9
Charlotte Campbell <32046951+charlottecampbell193@users.noreply.github.com>
2024-09-19 16:27:43 +1000

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -2,6 +2,6 @@ docker build -f Dockerfile.ci -t ${DOCKER_IMAGE_NAME} .
 docker tag ${DOCKER_IMAGE_NAME}:latest 192.168.143.128:5000/${DOCKER_IMAGE_NAME}:latest
 cmd_output=$(docker push 192.168.143.128:5000/${DOCKER_IMAGE_NAME}:latest)
 echo ${cmd_output}
-digest=$(echo "$cmd_output" | sed -n 's/.*digest: \(.*\)$/\1/p')
+digest=$(echo "$cmd_output" | sed -n 's/.*digest: \(sha256:[a-f0-9]\{64\}\).*/\1/p')
 echo ${digest}
 cosign sign --yes --key hashivault://test-key 192.168.143.128:5000/${DOCKER_IMAGE_NAME}:${digest}
